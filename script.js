d578214f982528a0fc6b941600f35dd2bfacca1b
ismaile <ismailearliene@gmail.com>
2022-12-08 15:22:00 -0300

    ClimaTempoComAPI

---
+++ script.js
@@ -0,0 +1,45 @@
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+
+const place = document.querySelector("#place");
+const degrees = document.querySelector("#degrees");
+const img = document.querySelector("img");
+const wind = document.querySelector("#wind");
+const content = document.querySelector(".content");
+const apiKey = '2d6b95b08bb5c891f8974ead6ad2eaa9'
+
+button.addEventListener("click", () => {
+  if (!input.value) return;
+
+  getDataApi();
+});
+
+async function getDataApi() {
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(
+    input.value
+  )}&units=metric&appid=${apiKey}`;
+
+  try {
+    await fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data?.cod && data.cod === "404") {
+          return alert("Local não encontrado!");
+        }
+
+        loadData(data);
+      });
+  } catch (error) {
+    alert(error);
+  }
+}
+
+function loadData(data) {
+  place.innerHTML = `${data.name}, ${data.sys.country}`;
+  degrees.innerHTML = `Temperatura: ${Math.floor(data.main.temp)}° C`;
+  img.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+  wind.innerHTML = `Vento: ${data.wind.speed} km/h`;
+  content.style.display = "flex";
+}
+
+
